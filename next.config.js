3fb75dbe1547558ef966a5c99b4f69ab8029c4da
N <nic@symbios.wiki>
2022-02-22 23:02:08 +0100

    Auto Deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
+  // distDir: 'build',
   exportPathMap: function() {
     return {
       "/symbios-wiki": {page: "/"}
