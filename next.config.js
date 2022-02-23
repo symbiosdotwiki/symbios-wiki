3786a81a3982f6022efc829df63c0e0efd65df71
N <nic@symbios.wiki>
2022-02-23 20:33:05 +0100

    Auto Deploy

--- next.config.js
+++ next.config.js
@@ -4,15 +4,15 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
   // distDir: 'build',
-  exportPathMap: function() {
-    if(isProd){
-      return {
-        "/symbios-wiki": {page: "/"}
-      }
-    }
-    return {}
-  },
-  assetPrefix: isProd ? '/symbios-wiki/' : '',
+  // exportPathMap: function() {
+  //   if(isProd){
+  //     return {
+  //       "/symbios-wiki": {page: "/"}
+  //     }
+  //   }
+  //   return {}
+  // },
+  // assetPrefix: isProd ? '/symbios-wiki/' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
